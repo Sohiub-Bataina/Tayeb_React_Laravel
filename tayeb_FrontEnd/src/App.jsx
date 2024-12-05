b47badcfda3c90522345b4b465f52f261448a38a
mays-alkhalil <mays-alkhalil>
2024-12-05 20:18:38 +0300

    push front

--- tayeb_FrontEnd/src/App.jsx
+++ tayeb_FrontEnd/src/App.jsx
@@ -1,30 +1,49 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Blogs from './components/Blogs';
 import CreateBlog from './components/CreateBlog';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import BlogDetail from './components/BlogDetail';
 import EditBlog from './components/EditBlog';
-
+import FavoritesPage from './components/FavoritesPage'; // صفحة المفضلة
 
 function App() {
+    const [favorites, setFavorites] = useState([]); // قائمة المفضلة
 
-  return (
-    <>
-        <div className='bg-dark text-center py-2 shadow-lg'>
-            <h1 className='text-white'>React & Laravel Blog App</h1>
-        </div>
-        <Routes>
-            <Route path='/' element={ <Blogs />} />
-            <Route path='/create' element={ <CreateBlog />} />
-            <Route path='/blog/:id' element={ <BlogDetail />} />
-            <Route path='/blog/edit/:id' element={ <EditBlog />} />
-        </Routes>   
-        <ToastContainer />   
-    </>
-  )
+    // دالة لإضافة/إزالة المدونات من المفضلة
+    const toggleFavorite = (blog) => {
+        setFavorites((prevFavorites) => {
+            if (prevFavorites.find((item) => item.id === blog.id)) {
+                return prevFavorites.filter((item) => item.id !== blog.id);
+            } else {
+                return [...prevFavorites, blog];
+            }
+        });
+    };
+
+    return (
+        <>
+            {/* الشريط العلوي */}
+            <div className="bg-dark text-center py-2 shadow-lg">
+                <h1 className="text-white">React & Laravel Blog App</h1>
+              
+            </div>
+
+            {/* التوجيه بين الصفحات */}
+            <Routes>
+                <Route path="/" element={<Blogs />} />
+                <Route path="/create" element={<CreateBlog />} />
+                
+                <Route path="/blog/:id" element={<BlogDetail onToggleFavorite={toggleFavorite} />} />
+                <Route path="/blog/edit/:id" element={<EditBlog />} />
+                <Route path="/favorites" element={<FavoritesPage favorites={favorites} />} />
+            </Routes>
+
+            <ToastContainer />
+        </>
+    );
 }
 
-export default App
+export default App;
--- tayeb_FrontEnd/src/components/BlogDetail.jsx
+++ tayeb_FrontEnd/src/components/BlogDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const BlogDetail = () => {
     const [blog, setBlog] = useState([]);
+    const [liked, setLiked] = useState(false); // حالة لتحديد إذا تم النقر على زر المفضلة
     const params = useParams();
 
     const fetchBlog = async () => {
@@ -11,33 +12,59 @@ const BlogDetail = () => {
         setBlog(result.data);
     }
 
-
     useEffect(() => {
         fetchBlog();
-    },[]);
+    }, []);
+
+    const toggleFavorite = () => {
+        setLiked(!liked); // عكس حالة المفضلة عند النقر
+    }
 
     return (
         <div className='container'>
-        <div className="d-flex justify-content-between pt-5 mb-4">
-            <h2>{blog.title}</h2>
-            <div>
-                <a href='/' className='btn btn-dark'>back to blogs</a>
+            <div className="d-flex justify-content-between align-items-center pt-5 mb-4">
+                {/* زر العودة */}
+                <a href='/' className='btn btn-dark'>Back to Blogs</a>
+
+                {/* زر المفضلة */}
+                <button className='btn' onClick={toggleFavorite}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill={liked ? "red" : "gray"} className="bi bi-heart" viewBox="0 0 16 16">
+                        <path d="M8 15s6-4.35 6-7.5C14 5.01 11.74 3 9 3c-1.74 0-3 1.51-3 1.51S4.74 3 3 3C.26 3 0 5.01 0 7.5 0 10.65 8 15 8 15z"/>
+                    </svg>
+                </button>
             </div>
-        </div>
-        <div className='row'>
-            <div className='col-md-12'>
-                <p>by <strong>{blog.author}</strong> on {blog.date}</p>
 
-                {
-                    (blog.image) && <img className='w-50' src={`http://localhost:8000/uploads/blogs/${blog.image}`} />
-                }
+            <div className="row mb-4">
+                <div className="col-md-8">
+                    {/* تفاصيل المدونة */}
+                    <h2>{blog.title}</h2>
+                    <p className="text-muted">by <strong>{blog.author}</strong> on {blog.date}</p>
 
-                <div className='mt-5' dangerouslySetInnerHTML={{ __html: blog.description }}>
+                    {blog.image && <img className='img-fluid mb-4' src={`http://localhost:8000/uploads/blogs/${blog.image}`} alt="Blog" />}
+
+                    {/* إضافة تنسيق للنص بشكل Inline */}
+                    <div 
+                        className="mt-4" 
+                        style={{
+                            wordWrap: 'break-word',  // لف الكلمات الطويلة
+                            whiteSpace: 'normal',    // التفاف النص بشكل طبيعي
+                            overflowWrap: 'break-word', // التأكد من التفاف النص بشكل صحيح
+                        }} 
+                        dangerouslySetInnerHTML={{ __html: blog.description }} 
+                    />
+                </div>
+                <div className="col-md-4">
+                    {/* يمكنك إضافة محتوى إضافي هنا إن أردت */}
+                    <h4>Related Blogs</h4>
+                    <ul>
+                        <li><a href="#">Blog 1</a></li>
+                        <li><a href="#">Blog 2</a></li>
+                        <li><a href="#">Blog 3</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
-export default BlogDetail
+export default BlogDetail;
