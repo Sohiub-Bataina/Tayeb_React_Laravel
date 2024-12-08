8cf7282c05d08d51af2d390b6bbba7d7a3907a25
Razanhroub <razan.b.alhroub@gmail.com>
2024-12-08 11:22:41 +0300

    fixing_button_color

--- tayeb_FrontEnd/src/components/Blogs.jsx
+++ tayeb_FrontEnd/src/components/Blogs.jsx
@@ -22,7 +22,7 @@ const Blogs = () => {
     const navigate = useNavigate();
     const goToCreate = () => {
         navigate("/create"); // ينقلك إلى صفحة Blogs
-      };
+    };
 
     // البحث عن مدونة
     const searchBlogs = async (e) => {
@@ -63,7 +63,7 @@ const Blogs = () => {
         <div className='container pt-5'>
             {/* الشريط العلوي */}
             <div className="row mb-4 align-items-center">
-            <div className="col-lg-6 col-md-8 col-sm-12 mb-3">
+                <div className="col-lg-6 col-md-8 col-sm-12 mb-3">
                     <form onSubmit={searchBlogs} className="d-flex">
                         <input
                             type="text"
@@ -74,9 +74,10 @@ const Blogs = () => {
                         />
                         <button className='btn btn-dark me-2'>Search</button>
                         <button
-                            type='button'
+                            type="button"
                             onClick={resetSearch}
-                            className='btn btn-success'
+                            style={{ backgroundColor: "#ee9750", color: "#fff", border: "none", important: true }}
+                            className="btn"
                         >
                             Reset
                         </button>
@@ -86,7 +87,7 @@ const Blogs = () => {
                     {/* <Link to="/favorites" state={{ favorites }} className='btn btn-danger me-2'>
                         Favorites
                     </Link> */}
-                   
+
                     <a onClick={goToCreate} className='btn btn-dark'>Create</a>
                 </div>
             </div>
